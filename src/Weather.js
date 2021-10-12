a0f7443ef208ec0e2812b6a6464f29c6f00d5791
JessAM10 <87210462+JessAM10@users.noreply.github.com>
2021-10-12 15:04:13 -0400

    Update API to only be called when needed

--- src/Weather.js
+++ src/Weather.js
@@ -1,61 +1,74 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  const apiKey = "0ced109d1b3107e21ab8ab47c9cb6bab";
-  let units = "metric";
-  let city = "Montreal";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  const [weather, setWeather] = useState({ ready: false });
 
   function showWeather(response) {
     console.log(response.data);
+    setWeather({
+      ready: true,
+      temperature: response.data.main.temp,
+      cityName: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+    });
   }
 
-  axios.get(apiUrl).then(showWeather);
-
-  return (
-    <div className="Weather">
-      <div className="container">
-        <form>
+  if (weather.ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <form>
+            <div className="row">
+              <div className="col-6">
+                <input
+                  className="search"
+                  type="search"
+                  placeholder="Search for a city"
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  className="btn btn-primary button"
+                  type="submit"
+                  value="Search"
+                />
+              </div>
+            </div>
+          </form>
+          <h1>{weather.cityName}</h1>
+          <ul>
+            <li>Saturday</li>
+            <li>Clear skies</li>
+          </ul>
           <div className="row">
-            <div className="col-6">
-              <input
-                className="search"
-                type="search"
-                placeholder="Search for a city"
+            <div className="col">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="sun with clouds"
               />
+              {Math.round(weather.temperature)} degrees
             </div>
-            <div className="col-6">
-              <input
-                className="btn btn-primary button"
-                type="submit"
-                value="Search"
-              />
+            <div className="col">
+              <ul>
+                <li>Humidity: {weather.humidity}%</li>
+                <li>Wind: 5 km/h</li>
+              </ul>
             </div>
-          </div>
-        </form>
-        <h1>Montreal</h1>
-        <ul>
-          <li>Saturday</li>
-          <li>Clear skies</li>
-        </ul>
-        <div className="row">
-          <div className="col">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="sun with clouds"
-            />
-            20 degrees
-          </div>
-          <div className="col">
-            <ul>
-              <li>Humidity: 50%</li>
-              <li>Wind: 5 km/h</li>
-            </ul>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "0ced109d1b3107e21ab8ab47c9cb6bab";
+    let units = "metric";
+    let city = "Paris";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showWeather);
+
+    return "Loading...";
+  }
 }
