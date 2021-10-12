de9334495eacd8f661f7991ec72733e04a8fb465
JessAM10 <87210462+JessAM10@users.noreply.github.com>
2021-10-12 12:44:28 -0400

    Deleted previous project and added the outline of new project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import Weather from "./Weather";
-// import Destinations from "./Destinations";
-import Search from "./Search";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Weather />
-        {/* <Destinations /> */}
-        <Search />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,77 +1,39 @@
 import React from "react";
 import "./Weather.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="container my-container">
-        <div className="row">
-          <div className="col-12 weather-title">
-            <h1>Weather</h1>
-            <h2>Around the World</h2>
-          </div>
-          <div className="col-6 current-location">
-            <span className="current-location-name"> Montreal </span>
-            <br />
-            <span className="last-updated">Last updated today</span>
-            <div className="current-location-description">
-              <span className="current-description">Clear</span>
-            </div>
-            <div className="col-4 feels-like">
-              <span className="feelsLike">Feels like 19</span>
-            </div>
-          </div>
-
+      <div className="container">
+        <form>
           <div className="row">
-            <div className="col-4 current-weather">
-              <img src="#" alt="" className="current-icon" />
-              <span className="current-temp">
-                <span>20</span>
-              </span>
-              <span className="units">
-                <a href="/" className="celsius-unit active">
-                  °C
-                </a>
-                |
-                <a href="/" className="fahrenheit-unit">
-                  °F
-                </a>
-              </span>
+            <div className="col-6">
+              <input className="search" type="search" placeholder="Search for a city" />
             </div>
-            <div className="col-3 align-items-start weather-details">
-              <ul>
-                <li>
-                  Humidity: <span>80%</span>
-                </li>
-                <li>
-                  Wind: <span>5 km/h</span>
-                </li>
-              </ul>
+            <div className="col-6">
+              <input className="btn btn-primary" type="submit" value="Search" />
             </div>
           </div>
-        </div>
-        <div className="forecast-icon"></div>
-        <div className="open-source">
-          <p>
-            <a
-              className="github-link"
-              href="https://github.com/JessAM10/my-weather-app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open-source code
-            </a>
-            by
-            <a
-              href="https://linkedin.com/in/jessallardm"
-              target="_blank"
-              rel="noreferrer"
-              className="linkedin"
-            >
-              Jessica Allard
-            </a>
-          </p>
+        </form>
+        <h1>Montreal</h1>
+        <ul>
+          <li>Saturday</li>
+          <li>Clear skies</li>
+        </ul>
+        <div className="row">
+          <div className="col">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="sun with clouds"
+            />
+            20 degrees
+          </div>
+          <div className="col">
+            <ul>
+              <li>Humidity: 50%</li>
+              <li>Wind: 5 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
