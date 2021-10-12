9e5215537eee3fdc6df8ebe4309cb48a0fc8859d
JessAM10 <87210462+JessAM10@users.noreply.github.com>
2021-10-12 17:01:40 -0400

    Replaced hard-coded weather details with real-time data from the API

--- src/Weather.js
+++ src/Weather.js
@@ -9,12 +9,14 @@ export default function Weather() {
     console.log(response.data);
     setWeather({
       ready: true,
-      temperature: response.data.main.temp,
       cityName: response.data.name,
-      humidity: response.data.main.humidity,
+      temperature: response.data.main.temp,
       description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` 
     });
+    
   }
 
   if (weather.ready) {
@@ -41,21 +43,18 @@ export default function Weather() {
           </form>
           <h1>{weather.cityName}</h1>
           <ul>
-            <li>Saturday</li>
-            <li>Clear skies</li>
+            {/* <li>Saturday</li> */}
+            <li className="text-capitalize">{weather.description}</li>
           </ul>
           <div className="row">
             <div className="col">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="sun with clouds"
-              />
-              {Math.round(weather.temperature)} degrees
+              <img src={weather.iconUrl} alt="weather icon" />
+              {Math.round(weather.temperature)}°C
             </div>
             <div className="col">
               <ul>
                 <li>Humidity: {weather.humidity}%</li>
-                <li>Wind: 5 km/h</li>
+                <li>Wind: {Math.round(weather.wind * 3.6)} km/h</li>
               </ul>
             </div>
           </div>
@@ -65,7 +64,7 @@ export default function Weather() {
   } else {
     const apiKey = "0ced109d1b3107e21ab8ab47c9cb6bab";
     let units = "metric";
-    let city = "Paris";
+    let city = "Montreal";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(showWeather);
 
