6abe56920835f29944f06d466ac32ca272f922df
JessAM10 <87210462+JessAM10@users.noreply.github.com>
2021-10-14 00:22:01 -0400

    Added link to github repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -7,8 +8,16 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather city="Montreal"/>
-      </header>
+        <Weather city="Montreal" />
+      </header>{" "}
+      <footer className="container">
+        {" "}
+        <a href="https://github.com/JessAM10/react-weather">
+          {" "}
+          Open-source code
+        </a>{" "}
+        by Jessica Allard
+      </footer>
     </div>
   );
 }
