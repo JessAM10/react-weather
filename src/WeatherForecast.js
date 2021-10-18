7530100f4e72e4151ef818ec95e9d2c051e860ec
JessAM10 <87210462+JessAM10@users.noreply.github.com>
2021-10-17 20:21:46 -0400

    Fixed weather icon  name and centered the forecast info

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,15 +3,18 @@ import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
+  
   return (
     <div className="WeatherForecast">
       <div className="container">
         <div className="row">
           <div clasName="col">
             <div>Tue</div>
-            <WeatherIcon code="01d" size={30} />
-            <span>20</span>
-            <span>10</span>
+            <WeatherIcon code="01d" size={40} />
+            <div className="forecast-tempearture">
+              <span>20° </span>
+              <span>10°</span>
+            </div>
           </div>
         </div>
       </div>
